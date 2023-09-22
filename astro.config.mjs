38120e7ed9dcfe65ee1fe5d2ca938fc0cd929699
Alexander Betz <42554018+alexanderbetz@users.noreply.github.com>
2023-09-22 23:33:36 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
-import vercelEdge from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercelEdge()
+  adapter: vercel({
+    edgeMiddleware: true
+  })
 });
